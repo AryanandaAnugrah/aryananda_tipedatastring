5acd6d79f8fb8fea887c5a9164cdb607e03172f7
AryanandaAnugrah <aryanandaa91@gmail.com>
2024-09-27 10:53:12 +0700

    Add files via upload

---
+++ tipe data string aryananda.py
@@ -0,0 +1,13 @@
+nama_depan = "Aryananda"
+nama_belakang = 'Anugrah'
+nama_lengkap = nama_depan + '' + nama_belakang
+usia = 19
+alamat = 'PangkalPinang'
+kata_mutiara = "Jangan lupa untuk tidur"
+
+print(nama_lengkap, '(' + usia + ')', ', ', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print(',Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
